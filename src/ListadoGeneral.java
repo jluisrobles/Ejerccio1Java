0f77ea7f25be41f571daac7e99d56c9dc95b7ae9
Robles <joseroblesarce@yahoo.es>
2023-02-07 18:20:44 +0000

    Ejercico resuelto

---
+++ src/ListadoGeneral.java
@@ -0,0 +1,40 @@
+import javax.swing.*;
+
+public class ListadoGeneral {
+    public static void main(String[] args) {
+
+        byte numero1 = 7;
+        System.out.println(numero1);
+        short numero2 = 17;
+        System.out.println(numero2);
+        int numero3 = 9;
+        System.out.println(numero3);
+        long numero4 = 369852;
+        System.out.println(numero4);
+        double variable1 = 123.12d;
+        System.out.println(variable1);
+
+
+        boolean resultado;
+                resultado = false;
+        if(resultado){
+        System.out.println("verdadero");
+         }
+        else{
+            System.out.println("falso");
+        String nombre = "Pedro";
+        String apellido = "Picaso";
+                System.out.println(nombre + " " + apellido);
+        }
+    }
+
+
+
+
+
+
+
+
+
+}
+
